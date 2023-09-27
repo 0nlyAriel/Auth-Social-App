5ff0ec4b761ccdc1723d175e8858cc6cc6db268c
Ariel Wobo <142696863+0nlyAriel@users.noreply.github.com>
2023-09-27 12:34:41 +0000

    Update 11 files

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import { Container } from "react-bootstrap";
 export default function App() {
   return (
     <div className="App">
-      <Container>
-        <Sigup />
+      <Container className="d-flex align-items-center 
+      justify-content-center"
+      style={{ minHeight:"100vh" }}>
+        <div className="w-100" style={{ maxWidth:'400px'}}>
+          <Sigup />
+        </div>
       </Container>
     </div>
   );
